84e8b1086156caf27ddcd3c6f1d97b402e5bde16
anand14 <anand@f22labs.com>
2019-06-12 23:51:41 +0530

    first commit

---
+++ precache-manifest.d826b1a365f08867d385fedf8c2b96d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7ea1b897c1eaba706bd3",
+    "url": "/static/js/main.7ea1b897.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "7ea1b897c1eaba706bd3",
+    "url": "/static/css/main.35872c8a.chunk.css"
+  },
+  {
+    "revision": "b492e556599e83e421288cf14b7566fa",
+    "url": "/index.html"
+  }
+];
